71a45fe4382d4682128444024391bfb82095a486
olololiy <olololiy@bk.ru>
2021-10-25 10:22:43 +0300

    remaking allNum

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-function checkPass(){
-
+function writeString(str){
+    document.getElementById("out").innerHTML = str
 }
 
-function allLetters(){
+function checkPass(){
+alert("checkpass")
+}
 
+function allLetters(param){
+    param.forEach(function(item, index, array) {//item это значение, а не указатель
+        if (allNum(item) === true) {
+            return(false)
+        } 
+      });
+      return(true)
 }
 
 function checkLetters(){
@@ -11,13 +20,13 @@ function checkLetters(){
 }
 
 function allNum(param){
-    return((Number(param) === NaN) ? false : true)
+    return((Number(param) === NaN) ? false : true)//wrong. 2,1 is number
 }
 
 function checkNum(param){
-    (param === 0) ? alert("Lalaley") : // да и все равно не выводит нужное wrong https://ru.wikipedia.org/wiki/%D0%A7%D1%91%D1%82%D0%BD%D0%BE%D1%81%D1%82%D1%8C_%D0%BD%D1%83%D0%BB%D1%8F#:~:text=%D0%9D%D0%BE%D0%BB%D1%8C%20%D0%B4%D0%B5%D0%BB%D0%B8%D1%82%D1%81%D1%8F%20%D0%BD%D0%B5%20%D1%82%D0%BE%D0%BB%D1%8C%D0%BA%D0%BE%20%D0%BD%D0%B0,%D1%87%D1%91%D1%82%D0%BD%D1%8B%D0%BC%C2%BB%20%D1%87%D0%B8%D1%81%D0%BB%D0%BE%D0%BC%20%D0%B8%D0%B7%20%D0%B2%D1%81%D0%B5%D1%85%20%D1%87%D0%B8%D1%81%D0%B5%D0%BB.
-    ((param % 2) === 0) ?  alert("Hop") :
-    alert("hey")
+    (param === 0) ? writeString("Lalaley") : // да и все равно не выводит нужное wrong https://ru.wikipedia.org/wiki/%D0%A7%D1%91%D1%82%D0%BD%D0%BE%D1%81%D1%82%D1%8C_%D0%BD%D1%83%D0%BB%D1%8F#:~:text=%D0%9D%D0%BE%D0%BB%D1%8C%20%D0%B4%D0%B5%D0%BB%D0%B8%D1%82%D1%81%D1%8F%20%D0%BD%D0%B5%20%D1%82%D0%BE%D0%BB%D1%8C%D0%BA%D0%BE%20%D0%BD%D0%B0,%D1%87%D1%91%D1%82%D0%BD%D1%8B%D0%BC%C2%BB%20%D1%87%D0%B8%D1%81%D0%BB%D0%BE%D0%BC%20%D0%B8%D0%B7%20%D0%B2%D1%81%D0%B5%D1%85%20%D1%87%D0%B8%D1%81%D0%B5%D0%BB.
+    ((param % 2) === 0) ?  writeString("Hop") :
+    writeString("Hey")
 }
 
 function checkParam(param){
@@ -36,7 +45,7 @@ function someFunc(){
     var quantity = document.getElementById("quantity").value;
     checkParam(quantity)
     //значение отправляем в другую функцию из 
-     alert(typeof(quantity))
+    //alert(typeof(quantity))
 
     // let a = quantity.length;
     // alert(a)
